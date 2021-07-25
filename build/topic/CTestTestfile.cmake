7039ce9b5b9a1c13c6e71faec5a34644e696a792
laphisboy <laphisboy@gmail.com>
2021-07-25 15:05:00 +0900

    f

---
+++ build/topic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/youngsun/catkin_ws/src/topic
+# Build directory: /home/youngsun/catkin_ws/build/topic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
